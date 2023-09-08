614c6f0694fa23d20844bc281ca2051e309ac3e9
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2023-09-08 11:57:14 -0500

    Create miztools.py

---
+++ miztools.py
@@ -0,0 +1,15 @@
+"""Create all tools classes for use in other projects."""
+import json
+from socket import gethostbyaddr
+class atools():
+  """A tools class used to append and get things to and from important save docs."""
+  def __init__(self, doc):
+    self.doc = doc
+  def append(self, userinput):
+    self.userinput = userinput
+    out_file = open(self.doc, "w")
+    json.dump(userinput, out_file, indent = 6)
+  def get(self, line = 1):
+    in_file = open(self.doc)
+    out = json.load(in_file)
+    return out
