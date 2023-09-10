baf50c2cea9fcf40905eb654df95e948f3e9c4ab
Marlon Kohn <126592834+mizaagi@users.noreply.github.com>
2023-09-10 07:38:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import miztools as mt
+import datetime as dt
 
+"""Set up json appendage tools."""
+f = mt.atools("umbrella_storage.json")
+dl_td = dt.datetime.now()
+
+"""Add installation date to OS."""
+f.append(f"Umbrella installed at {dl_td.month}/{dl_td.day}/{dl_td.year} at {dl_td.hour} hours, 
+{dl_td.minute} minutes, {dl_td.second} seconds and {dl_td.microsecond} microseconds GMT.")
+
+f.append(f'{dl_td.month}/{dl_td.day}/{dl_td.year}')
